53380e1b1bb5a9d73c34e656ce2cfbe16a1c4f2d
Tommymuia <muiathomas.mt@gmail.com>
2026-01-14 07:16:31 +0300

    updated JWT logic

--- app/security.py
+++ app/security.py
@@ -1,6 +1,12 @@
 from passlib.context import CryptContext
 pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+
+SECRET_KEY = "your-super-secret-key-here" 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 def hash_pin(pin:str):
     return pwd_context.hash(pin)
 
