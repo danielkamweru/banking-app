067a15bb02f374baa52d0dcad551d11d4ad3f018
Tommymuia <muiathomas.mt@gmail.com>
2026-01-14 06:29:40 +0300

    added pin hashing logic

---
+++ app/security.py
@@ -0,0 +1,8 @@
+from passlib.context import CryptContext
+pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_pin(pin:str):
+    return pwd_context.hash(pin)
+
+def verify_pin(plain_pin:str,hashed_pin:str):
+    return pwd_context.verify(plain_pin,hashed_pin)
