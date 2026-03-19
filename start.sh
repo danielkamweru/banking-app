8f51af486275ab6da6a9bf70f9b46b05c79be78d
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-03-19 13:08:25 +0300

    feat: add start script with auto port cleanup

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/user/Development/code/fullstack/Banking-App
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+fuser -k 8000/tcp 2>/dev/null
+uvicorn app.main:app --reload --port 8000
